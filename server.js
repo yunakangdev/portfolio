ee85a2e0425cd55337fe02b2976b2068e5f878f7
Jean Kang <jeankang.dev@gmail.com>
2020-12-28 07:28:04 -0500

    add hbs

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars');
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.use(express.static('./views/images/'));
+
+app.get('/', (req, res) => {
+res.render('index', {layout: false})
+})
+
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`)
+})
