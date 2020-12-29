30aa84ef44f2b37a38a29755d73ba50d503e22c9
Jean Kang <jeankang.dev@gmail.com>
2020-12-28 22:59:28 -0500

    solve art direction problem

--- main.js
+++
@@ -1,8 +0,0 @@
-function changeAboutImg() {
-  if (window.innerWidth >= 368) {
-    document.getElementsByClassName("about__image").src="about_web.png";
-  }
-  else {
-    document.getElementsByClassName("about__image").src="about_mobile.png";
-  }
-}
