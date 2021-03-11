9fa025de7e337d2356aeb6e79412caa3a9ca93e6
Jean Kang <jeankang.dev@gmail.com>
2021-03-10 21:57:26 -0500

    add linkedin

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const exphbs = require('express-handlebars');
 
+const exphbs = require('express-handlebars');
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.use(express.static('./views/'));
