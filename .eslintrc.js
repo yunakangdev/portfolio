b75186c098cc403b3c32debab56521abd999c0c2
Jean Kang <jeankang.dev@gmail.com>
2021-04-20 07:57:40 -0400

    install eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,13 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true,
-        "node": true
+        "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+    "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
-      "no-console": 0,
-      "indent": ["warning", 2],
-      "no-unused-vars": 1,
-      "no-use-before-define": 1,
-      "no-redeclare": 1,
-      "no-console": 0,
     }
 };
-
